877a22899f447a07d2c12a9afbb839c3d54552e5
Ceefar <rob.farq@googlemail.com>
2022-10-15 01:07:53 +0100

    new version for project rework

--- v2/CleanFork_P10/main.py
+++ v2/CleanFork_P10/main.py
@@ -1,13 +1,10 @@
-# KidsCanCode - Game Development with Pygame video series
-# Tile-based game - Part 10
-# Player and Mob Health
-# Video link: https://youtu.be/-9bXcSjuN28
 import pygame as pg
 import sys
 from os import path
 from settings import *
 from sprites import *
 from tilemap import *
+
 
 # HUD functions
 def draw_player_health(surf, x, y, pct):
@@ -44,6 +41,21 @@ class Game:
         self.mob_img = pg.image.load(path.join(img_folder, MOB_IMG)).convert_alpha()
         self.wall_img = pg.image.load(path.join(img_folder, WALL_IMG)).convert_alpha()
         self.wall_img = pg.transform.scale(self.wall_img, (TILESIZE, TILESIZE))
+        # load fonts
+        # choices
+        # ka1.ttf # Silkscreen-Regular.ttf # upheavtt.ttf # Daydream.ttf # superstar_memesbruh03.ttf # Kappa_ (various)
+        self.FONT_SILK_REGULAR_10 = pg.font.Font("Silkscreen-Regular.ttf", 10) # more here than needed to test, remove those unused when ui is finalised 
+        self.FONT_SILK_REGULAR_12 = pg.font.Font("Silkscreen-Regular.ttf", 12)
+        self.FONT_SILK_REGULAR_14 = pg.font.Font("Silkscreen-Regular.ttf", 14)
+        self.FONT_SILK_REGULAR_16 = pg.font.Font("Silkscreen-Regular.ttf", 16)
+        self.FONT_SILK_REGULAR_18 = pg.font.Font("Silkscreen-Regular.ttf", 18)
+        self.MEMESBRUH_32 = pg.font.Font("superstar_memesbruh03.ttf", 32) 
+        self.MEMESBRUH_36 = pg.font.Font("superstar_memesbruh03.ttf", 36) 
+        self.MEMESBRUH_44 = pg.font.Font("superstar_memesbruh03.ttf", 44)
+        # new test concept for drawing damage numbers on screen
+        self.damage_numbers_positions_list = []
+        self.damage_numbers_pos_timers_list = []
+        self.random_rotation = 0
 
     def new(self):
         # initialize all variables and do all the setup for a new game
@@ -78,7 +90,7 @@ class Game:
         # update portion of the game loop
         self.all_sprites.update()
         self.camera.update(self.player)
-        # mobs hit player
+        # -- mobs hit player --
         hits = pg.sprite.spritecollide(self.player, self.mobs, False, collide_hit_rect)
         for hit in hits:
             self.player.current_health -= MOB_DAMAGE
@@ -87,10 +99,18 @@ class Game:
                 self.playing = False
         if hits:
             self.player.pos += vec(MOB_KNOCKBACK, 0).rotate(-hits[0].rot)
-        # bullets hit mobs
+        # -- for test implementation of damage numbers display, needs to be above bullets hit mobs otherwise the mobs may die before printing the number --
+        # -- bullets hit mobs --
+        hits = pg.sprite.groupcollide(self.bullets, self.mobs, False, False)
+        for hit in hits:
+            if hit.pos not in self.damage_numbers_positions_list: # dont add the same position and print loads over each other for no reason
+                self.damage_numbers_positions_list.append(hit.pos) # append it then check if its near, if it is too close to one already then remove it
+                self.damage_numbers_pos_timers_list.append(pg.time.get_ticks())
+        # -- mobs hit bullets --
         hits = pg.sprite.groupcollide(self.mobs, self.bullets, False, True)
         for hit in hits:
-            hit.current_health -= BULLET_DAMAGE
+            self.randomised_bullet_damage = int((BULLET_DAMAGE / 100) * randint(80, 120))# regardless of what the damage will be give it a 80 - 100% range
+            hit.current_health -= self.randomised_bullet_damage
             hit.vel = vec(0, 0)
 
     def draw_grid(self):
@@ -104,12 +124,53 @@ class Game:
         self.screen.fill(BGCOLOR)
         # self.draw_grid()
         for sprite in self.all_sprites:
+            # zombie mob sprites
             if isinstance(sprite, Mob):
-                sprite.draw_unit_health() # sprite.draw_health()
+                # draw unit health with hp block segments
+                sprite.draw_unit_health() # previously, sprite.draw_health()
+                # draw level box with level number
+                sprite.draw_unit_level()
+                # draw the units name stationary within the health and level bounds, note - is different from OG draw_name which drew the name directly underneath with some rotation considerations
+                sprite.draw_unit_name()
+                # draw the unit status
+                sprite.draw_unit_status()
+                # draw the unit action chargebar, only activating charging an attack when the player is close
+                distance_to_player = how_near(sprite, self.player.pos.x, self.player.pos.y)
+                if distance_to_player < 150:
+                    # if zombie is close and theres no timer start the timer
+                    if not sprite.attack_timer:
+                        sprite.attack_timer = pg.time.get_ticks()
+                    # else if the zombie is close and the timer is running      
+                    else:
+                        check_timer = pg.time.get_ticks()
+                        true_timer = check_timer - sprite.attack_timer
+                        # if its on and over 2000 reset it
+                        if true_timer >= 1500:
+                            sprite.attack_timer = 0
+                        sprite.draw_unit_action_chargebar((true_timer / 1500) * 97)
+                else:
+                    sprite.draw_unit_action_chargebar(0)
+            # blit the sprite to the screen
             self.screen.blit(sprite.image, self.camera.apply(sprite))
-        # pg.draw.rect(self.screen, WHITE, self.player.hit_rect, 2)
+        # -- for test implementation of displaying damage numbers --
+        if self.damage_numbers_positions_list:
+            for position in self.damage_numbers_positions_list:
+                self.draw_damage_numbers(position, self.randomised_bullet_damage)
+                # print(f"{position = }")
         # HUD functions
         draw_player_health(self.screen, 10, 10, self.player.current_health / PLAYER_HEALTH)
+        # to only have damage numbers be on screen for a short amount of time
+        for i, position in enumerate(self.damage_numbers_positions_list): # everything except the one u just added
+            check_timer = pg.time.get_ticks()
+            on_screen_time = check_timer - self.damage_numbers_pos_timers_list[i]
+            if on_screen_time > 200:
+                self.damage_numbers_positions_list.pop(i)
+                self.damage_numbers_pos_timers_list.pop(i)
+        # to draw player hitbox rect
+        want_player_hitbox = False
+        if want_player_hitbox:
+            pg.draw.rect(self.screen, WHITE, self.player.hit_rect, 2) 
+        # finally, flip the display
         pg.display.flip()
 
     def events(self):
@@ -126,6 +187,60 @@ class Game:
 
     def show_go_screen(self):
         pass
+
+
+    # ---- new custom functions ----
+
+    # needs slight persistance
+    # randomise the positions slightly
+    # maybe add some transparency
+    def draw_damage_numbers(self, position, damage=100): # [CUSTOM]
+        self.damage_number_text_surface = self.MEMESBRUH_32.render(f"{damage}", True, WHITE).convert_alpha() # "text", antialias, color
+        # different colour border for higher numbers, do this for crits surely, or atleast use the concept
+        if damage > 50:
+            colr = RED
+        else:
+            colr = BLUEMIDNIGHT
+        self.damage_number_background_surface = self.MEMESBRUH_32.render(f"{damage}", True, colr).convert_alpha() # MEMESBRUH_44
+        x, y = position.x, position.y
+        destination_rect = pg.Rect(x, y, 300, 100) # a temporary rect to store the x, y positions we want to be at, so we can adjust it for the camera
+        destination = self.camera.apply_to_rect(destination_rect).copy()
+        background_destination = self.camera.apply_to_rect(destination_rect).copy()
+        # draw the background, i.e. the border when centralised
+        # print(f"{self.damage_number_background_surface.get_width() = }, {self.damage_number_background_surface.get_width() * 1.2 = }")
+        self.damage_number_background_surface = pg.transform.scale(self.damage_number_background_surface, (self.damage_number_background_surface.get_width() * 1.2, self.damage_number_background_surface.get_height() * 1.3))
+        background_destination.move_ip(10, 0) # (0,0) # best way to handle moving things in place => _ip
+        self.damage_number_background_surface.set_alpha(200)
+        self.screen.blit(self.damage_number_background_surface, background_destination)
+        # draw the foreground
+        destination.move_ip(14, 4) # (+4, +4) # best way to handle moving things in place => _ip
+        self.damage_number_text_surface.set_alpha(200)
+        self.screen.blit(self.damage_number_text_surface, destination)  
+
+        # THEN YOU JUST STORE ANOTHER LIST WITH EACH DICTS TIMER
+        # AND WHEN EACH TIMER HITS ITS MAX
+        # YOU REMOVE THE ITEM FROM THE LIST
+        # CHEFSKISS.PNG
+        # THEN
+        # FINISH ZOMBIE BAR UI STUFF (status/statuses, chargebar below hit for hits activate when close and stops at full fine for now, blue bar below that that can be toggled dull/grey)
+        # THEN
+        # NEW PLAYER SPRITE
+        # NEW ZOMBIE SPRITE
+        # NEW ARMORED ZOMBIE SPRITE OR SUMNT SIMILAR WHICH CAN JUST PUT AT LIKE LVL 5 FOR NOW OR SUMNT 
+        # PUT BAR ON PLAYER
+
+        # THEN FOR TOMO IG MAYBE
+        # START WITH ADDING CLEAN COMPANION?
+        # TBF SEE PHONE NOTES 
+        
+        
+        
+    # for numbers display idea
+    # store a list of numbers that you want to display with their positions
+    # display them
+    # this would be good as would allow for timing too so they could persist past the bullets death / kill
+    # you just empty them at a certain rate, doesnt even have to be a timer a simple countdown will work too tbf, quickest way to test too
+        
 
 # create the game object
 g = Game()
--- v2_history/v2_01_withOgNamePrintingUnderZombiesCodeStill/CleanFork_P10/sprites.py
+++ v2_history/v2_01_withOgNamePrintingUnderZombiesCodeStill/CleanFork_P10/sprites.py
@@ -2,8 +2,6 @@ import pygame as pg
 from random import uniform, randint
 from settings import *
 from tilemap import collide_hit_rect
-# New
-from math import hypot 
 vec = pg.math.Vector2
 
 ###########################################
@@ -14,10 +12,9 @@ def round_to_base(x, base=5): # [CUSTOM]
     """ defaults to 5 """
     return base * round(x/base)
 
-def how_near(self, x, y): # [CUSTOM]
-    # use hypotenus of the xy vectors to find out how close we are to the given vector pos
-    pythag_dist = hypot(self.pos.x-x, self.pos.y-y)
-    return pythag_dist
+
+
+###########################################
 
 def collide_with_walls(sprite, group, dir):
     if dir == 'x':
@@ -38,10 +35,6 @@ def collide_with_walls(sprite, group, dir):
                 sprite.pos.y = hits[0].rect.bottom + sprite.hit_rect.height / 2
             sprite.vel.y = 0
             sprite.hit_rect.centery = sprite.pos.y
-
-
-###########################################
-
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game, x, y):
@@ -93,9 +86,7 @@ class Player(pg.sprite.Sprite):
         self.rect.center = self.hit_rect.center
 
 
-class Mob(pg.sprite.Sprite): # heremob herezombie
-    zombie_counter = 1 # class var for ids
-
+class Mob(pg.sprite.Sprite):
     def __init__(self, game, x, y):
         self.groups = game.all_sprites, game.mobs
         pg.sprite.Sprite.__init__(self, self.groups)
@@ -110,35 +101,13 @@ class Mob(pg.sprite.Sprite): # heremob herezombie
         self.rect.center = self.pos
         self.rot = 0
         # new custom variables
-        self.myname = self.get_first_name()
-        self.power_level = self.set_power_level()
-        # important! => starting health is reliant on power_level
-        self.max_hp_amount = self.set_max_hp_amount() # self.current_health = MOB_HEALTH
-        self.min_hp_amount = self.set_min_hp_amount()
+        self.max_hp_amount = 350 # self.current_health = MOB_HEALTH
+        self.min_hp_amount = 200
         self.max_health = round_to_base(self.set_initial_health())
         self.current_health = self.max_health # should rename to 'current health' asap btw to avoid confusion
-        # more new custom variables
-        self.myid = Mob.zombie_counter
-        Mob.zombie_counter += 1
-        self.my_status = self.get_status()
-        # new for boolean on hit idea, rn testing for chargebar tho
-        self.attack_timer = 0
-        # debuggy init values
-        print(f"{self.myname}: {self.max_hp_amount = }, {self.min_hp_amount = }, {self.max_health = }, LVL={self.power_level}")
-
-    def set_max_hp_amount(self): # [CUSTOM]
-        base_max_hp_amount = 200
-        level_adjusted_max_hp_amount = base_max_hp_amount + (50 * (self.power_level - 1)) # -1 so at level 1 there is no additional bonus but is so minor it can be changed tbf
-        return level_adjusted_max_hp_amount
-
-    def set_min_hp_amount(self): # [CUSTOM]
-        base_min_hp_amount = 200
-        level_adjusted_min_hp_amount = base_min_hp_amount + (50 * (self.power_level - 1)) # -1 so at level 1 there is no additional bonus but is so minor it can be changed tbf
-        return level_adjusted_min_hp_amount
-
-    def set_power_level(self): # [CUSTOM]
-        # guna do properly shortly, for now just a random range is fine say 1 - 5
-        return randint(1,5) # ideally would be like 1 - 2/3/4 but idk how incremental we are guna get with it and what the max is yet so dw
+        self.myname = self.get_first_name()
+        # new custom fonts
+        self.FONT_SILK_REGULAR_12 = pg.font.Font("Silkscreen-Regular.ttf", 12)
 
     def update(self):
         self.rot = (self.game.player.pos - self.pos).angle_to(vec(1, 0))
@@ -157,15 +126,6 @@ class Mob(pg.sprite.Sprite): # heremob herezombie
         if self.current_health <= 0:
             self.kill()
 
-    def get_status(self): # [CUSTOM]
-        # kinda just as temp af rn so can randomise the status just to see how the display will look at certain char widths, not intended to be planned implementation of statuses
-        list_of_statuses = ["Roaming", "Hunting"]
-        roll = randint(0,2)
-        if roll == 2:
-            return list_of_statuses
-        else:
-            return list_of_statuses[roll]
-
     def get_first_name(self): # [CUSTOM]
         list_of_names = ["Zob", "Zenjamin", "Zames", "Zohn", "Zichard", "Zhomas", "Zhristopher", "Zaniel", "Znthony"]
         return(list_of_names[randint(0, 8)])
@@ -178,7 +138,7 @@ class Mob(pg.sprite.Sprite): # heremob herezombie
     # HUD functions
     def draw_unit_level(self): # [CUSTOM]
         BOX_SIZE = 28 # current box size = 20, note this should be slightly bigger than the unit health bar size btw so when hardcoding do this properly, aka dynamic from it
-        x, y = (self.pos.x + int(TILESIZE/2) - 30) + int(TILESIZE/2), self.pos.y - int(TILESIZE/2) - 6 - 10 # scoot back and up a tad from the unit health display to be centralised
+        x, y = (self.pos.x + int(TILESIZE/2) - 30) + int(TILESIZE/2), self.pos.y - int(TILESIZE/2) - 4 - 10 # scoot back and up a tad from the unit health display to be centralised
         level_box_fill = pg.Rect(x, y, BOX_SIZE, BOX_SIZE)
         level_box_outline_rect = pg.Rect(x, y, BOX_SIZE, BOX_SIZE)
         # before we draw it apply the camera to it, the move ip does nothing but left incase in future we wanna reposition the bars, use move ip instead
@@ -188,50 +148,31 @@ class Mob(pg.sprite.Sprite): # heremob herezombie
         level_box_outline_rect.move_ip(0, 0) # better way to handle moving things btw donkey!, also btw, ip = in place
         pg.draw.rect(self.game.screen, BLUEMIDNIGHT, level_box_fill)
         pg.draw.rect(self.game.screen, DARKGREY, level_box_outline_rect, 3)
-        # finally draw the actual number representing this unit/zombies level in that box
-        self.level_textsurface = self.game.FONT_SILK_REGULAR_18.render(f"{self.power_level}", True, WHITE) # "text", antialias, color
-        temp_rect = pg.Rect(x, y, 300, 100) # a temporary rect to store the x, y positions we want to be at, so we can adjust it for the camera
-        destination = self.game.camera.apply_to_rect(temp_rect).copy()
-        destination.move_ip(BOX_SIZE/2 - 7, 1)
-        self.game.screen.blit(self.level_textsurface, destination)
 
-    # add a char limit to this once implementing statuses
-    def draw_unit_status(self): # [CUSTOM]
-        status = self.my_status if isinstance(self.my_status, str) else ", ".join(self.my_status)
-        self.name_textsurface = self.game.FONT_SILK_REGULAR_10.render(f"{status}", True, RED) # "text", antialias, color
-        x, y = self.pos.x + int(TILESIZE/2) + int(TILESIZE/2), self.pos.y - int(TILESIZE/2) - 10
-        test_rect = pg.Rect(x, y, 300, 100) # a temporary rect to store the x, y positions we want to be at, so we can adjust it for the camera
-        destination = self.game.camera.apply_to_rect(test_rect).copy()
-        destination.move_ip(2, -30) # better way to handle moving things btw donkey!, also btw, ip = in place
-        self.game.screen.blit(self.name_textsurface, destination)
-
-    def draw_unit_action_chargebar(self, pct=97):
-        BAR_LENGTH = 97
-        BAR_HEIGHT = 4 # 20
-        x, y = self.pos.x + int(TILESIZE/2) + int(TILESIZE/2), self.pos.y - int(TILESIZE/2) - 10 
-        if pct > BAR_LENGTH: # just incase we get a number that is bigger
-            pct = BAR_LENGTH
-        chargebar = pg.Rect(x, y, pct, BAR_HEIGHT)
-        chargebar = self.game.camera.apply_to_rect(chargebar).copy()
-        chargebar.move_ip(2,17) # (-10, TILESIZE/2) # better way to handle moving things btw donkey!, also btw, ip = in place
-        chargebar_background = pg.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
-        chargebar_background = self.game.camera.apply_to_rect(chargebar_background).copy()
-        chargebar_background.move_ip(2,17) # (-10, TILESIZE/2) # better way to handle moving things btw donkey!, also btw, ip = in place
-        pg.draw.rect(self.game.screen, GREY, chargebar_background)
-        pg.draw.rect(self.game.screen, ORANGE, chargebar)
+    def draw_name(self): # [CUSTOM]
+        # then before we draw the name rotate it to where we want it to be, since we're doing it with blit in relation to the camera
+        self.name_textsurface = self.FONT_SILK_REGULAR_12.render(f"{self.myname}", True, BLACK) # (f"{self.myname} {self.health}", True, BLACK)  # "text", antialias, color
+        # e.g. this will rotate to face the player => pg.transform.rotate(textsurface, self.game.player.rot)
+        #if self.rot > -135 and self.rot < -45: # only do our rotation at certain angles based on the zombie
+        self.name_textsurface = pg.transform.rotate(self.name_textsurface, self.rot + 90) # if at this angle rotate my name
+        return self.name_textsurface
 
     def draw_unit_name(self): # [CUSTOM]
-        self.name_textsurface = self.game.FONT_SILK_REGULAR_14.render(f"{self.myname}", True, WHITE) # "text", antialias, color
+        # then before we draw the name rotate it to where we want it to be, since we're doing it with blit in relation to the camera
+        self.name_textsurface = self.FONT_SILK_REGULAR_12.render(f"{self.myname}", True, BLACK) # (f"{self.myname} {self.health}", True, BLACK)  # "text", antialias, color
+        # e.g. this will rotate to face the player => pg.transform.rotate(textsurface, self.game.player.rot)
+        #if self.rot > -135 and self.rot < -45: # only do our rotation at certain angles based on the zombie
+        # self.name_textsurface = pg.transform.rotate(self.name_textsurface, self.rot + 90) # if at this angle rotate my name
+        # return self.name_textsurface
         x, y = self.pos.x + int(TILESIZE/2) + int(TILESIZE/2), self.pos.y - int(TILESIZE/2) - 10
-        test_rect = pg.Rect(x, y, 300, 100) # a temporary rect to store the x, y positions we want to be at, so we can adjust it for the camera
+        test_rect = pg.Rect(x, y, 300, 100)
         destination = self.game.camera.apply_to_rect(test_rect).copy()
-        destination.move_ip(2, -20) # better way to handle moving things btw donkey!, also btw, ip = in place
         self.game.screen.blit(self.name_textsurface, destination)
 
     def draw_unit_health(self): # [CUSTOM]
         # basic bar setup for mob character
         BAR_LENGTH = 100
-        BAR_HEIGHT = 16 # 20
+        BAR_HEIGHT = 20
         # for strips between bars, serves to indicate a block/chunk of hp and ** not ** relative to the bar length
         BAR_HP_SEGMENT = 50
         BAR_SEGMENT_COUNT = self.max_health / BAR_HP_SEGMENT
@@ -266,33 +207,25 @@ class Mob(pg.sprite.Sprite): # heremob herezombie
                 # ------------ THIS DOESNT WORK AND IDK WHY BUT JUST MOVING ON FOR NOW :( ------------ #
                 # for segment bar height have longer/shorter for 50s and 100s
                 if i+1 % 2 == 0:
-                    segment_height = 4
+                    # it is even, so make it longer
+                    segment_rect = pg.Rect(x + (SEGMENT_LENGTH * (i+1)), y, 2, (BAR_HEIGHT / 4)) # for width -> 2 = thin, 1 = waffer thin, 4 = pretty chunky 
+                    # again before drawing, ensure we move these bars based on the cameras position too
+                    segment_rect = self.game.camera.apply_to_rect(segment_rect).copy()                    
+                    pg.draw.rect(self.game.screen, DARKGREY, segment_rect, 2)                   
                 else:
-                    segment_height = 2
-                # it is even, so make it longer
-                segment_rect = pg.Rect(x + (SEGMENT_LENGTH * (i+1)), y, 2, (BAR_HEIGHT / segment_height)) # for width -> 2 = thin, 1 = waffer thin, 4 = pretty chunky 
-                # again before drawing, ensure we move these bars based on the cameras position too
-                segment_rect = self.game.camera.apply_to_rect(segment_rect).copy()                    
-                pg.draw.rect(self.game.screen, DARKGREY, segment_rect, 2)                   
+                    # it is odd, so make it shorter
+                    segment_rect = pg.Rect(x + (SEGMENT_LENGTH * (i+1)), y, 2, (BAR_HEIGHT / 2)) # for width -> 2 = thin, 1 = waffer thin, 4 = pretty chunky
+                    # again before drawing, ensure we move these bars based on the cameras position too
+                    segment_rect = self.game.camera.apply_to_rect(segment_rect).copy()       
+                    pg.draw.rect(self.game.screen, DARKGREY, segment_rect, 2)
         # for debuggy
         want_debuggy = False
         if want_debuggy:
             print(f"{self.myname.title()} : hp blocks = {BAR_SEGMENT_COUNT}, {self.current_health}hp of {self.max_health = }hp remaining")
             print(f"{i+1 = }, {(i+1) * SEGMENT_LENGTH = }, {BAR_SEGMENT_COUNT = }, {int(BAR_SEGMENT_COUNT) = }")
-       
-    def draw_name(self): # [DEPRECIATED] [CUSTOM]
-        """ og way, draws the image underneath the zombie with some light regard to its rotation """
-        # then before we draw the name rotate it to where we want it to be, since we're doing it with blit in relation to the camera
-        self.name_textsurface = self.game.FONT_SILK_REGULAR_12.render(f"{self.myname}", True, BLACK) # (f"{self.myname} {self.health}", True, BLACK)  # "text", antialias, color
-        # e.g. this will rotate to face the player => pg.transform.rotate(textsurface, self.game.player.rot)
-        #if self.rot > -135 and self.rot < -45: # only do our rotation at certain angles based on the zombie
-        self.name_textsurface = pg.transform.rotate(self.name_textsurface, self.rot + 90) # if at this angle rotate my name
-        return self.name_textsurface
+        
 
-
-class Bullet(pg.sprite.Sprite): # herebullet
-    bullet_counter = 1
-
+class Bullet(pg.sprite.Sprite):
     def __init__(self, game, pos, dir):
         self.groups = game.all_sprites, game.bullets
         pg.sprite.Sprite.__init__(self, self.groups)
@@ -304,9 +237,6 @@ class Bullet(pg.sprite.Sprite): # herebullet
         spread = uniform(-GUN_SPREAD, GUN_SPREAD)
         self.vel = dir.rotate(spread) * BULLET_SPEED
         self.spawn_time = pg.time.get_ticks()
-        # more new custom variables
-        self.myid = Bullet.bullet_counter
-        Bullet.bullet_counter += 1
 
     def update(self):
         self.pos += self.vel * self.game.dt
@@ -314,10 +244,9 @@ class Bullet(pg.sprite.Sprite): # herebullet
         if pg.sprite.spritecollideany(self, self.game.walls):
             self.kill()
         if pg.time.get_ticks() - self.spawn_time > BULLET_LIFETIME:
-            self.kill()           
+            self.kill()
 
-
-class Wall(pg.sprite.Sprite): # herewall
+class Wall(pg.sprite.Sprite):
     def __init__(self, game, x, y):
         self.groups = game.all_sprites, game.walls
         pg.sprite.Sprite.__init__(self, self.groups)
